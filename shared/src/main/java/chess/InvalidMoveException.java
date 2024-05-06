38c7b8162b983b7f4821af226661e27a8fdfdf59
matthewandersonthompson <matthewandersonthompson@gmail.com>
2024-05-05 22:53:36 -0600

    Again, I realized that I didn't need to edit this one yet, so I just reverted it back to its og state

--- shared/src/main/java/chess/InvalidMoveException.java
+++ shared/src/main/java/chess/InvalidMoveException.java
@@ -1,11 +1,11 @@
 package chess;
 
-
+/**
+ * Indicates an invalid move was made in a game
+ */
 public class InvalidMoveException extends Exception {
-    
-    public InvalidMoveException() {
-        super("Invalid move");
-    }
+
+    public InvalidMoveException() {}
 
     public InvalidMoveException(String message) {
         super(message);
