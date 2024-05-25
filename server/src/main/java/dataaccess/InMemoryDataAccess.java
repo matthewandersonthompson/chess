4733e09a8a66595e748ba371ceab482440feacfe
matthewandersonthompson <matthewandersonthompson@gmail.com>
2024-05-25 16:55:27 -0600

    Added the DataAccessInterface and InMemoryDataAccess files

---
+++ server/src/main/java/dataaccess/InMemoryDataAccess.java
@@ -0,0 +1,91 @@
+package dataaccess;
+
+import model.UserData;
+import model.GameData;
+import model.AuthData;
+
+import java.util.*;
+
+public class InMemoryDataAccess implements DataAccessInterface {
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+
+    @Override
+    public void clear() {
+        users.clear();
+        games.clear();
+        authTokens.clear();
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User already exists");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        UserData user = users.get(username);
+        if (user == null) {
+            throw new DataAccessException("User not found");
+        }
+        return user;
+    }
+
+    @Override
+    public void createGame(GameData game) throws DataAccessException {
+        if (games.containsKey(game.gameID())) {
+            throw new DataAccessException("Game already exists");
+        }
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        GameData game = games.get(gameID);
+        if (game == null) {
+            throw new DataAccessException("Game not found");
+        }
+        return game;
+    }
+
+    @Override
+    public List<GameData> listGames() {
+        return new ArrayList<>(games.values());
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        if (!games.containsKey(game.gameID())) {
+            throw new DataAccessException("Game not found");
+        }
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        if (authTokens.containsKey(auth.authToken())) {
+            throw new DataAccessException("Auth token already exists");
+        }
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        AuthData auth = authTokens.get(authToken);
+        if (auth == null) {
+            throw new DataAccessException("Auth token not found");
+        }
+        return auth;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        if (authTokens.remove(authToken) == null) {
+            throw new DataAccessException("Auth token not found");
+        }
+    }
+}
