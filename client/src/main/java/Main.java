8b1853eb2f8db0e21330c31ff5931afac6378d5e
matthewandersonthompson <matthewandersonthompson@gmail.com>
2024-06-08 18:55:36 -0400

    Fixed a compilation error. My Server file couldn't be seen my my client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,21 @@
-import chess.*;
+package main;
+
+import chess.ChessGame;
+import chess.ChessPiece;
+import client.ServerFacade;
+import server.Server;
+import ui.PreloginUI;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        Server server = new Server();
+        server.run(8080);
+
+        ServerFacade serverFacade = new ServerFacade("localhost", 8080);
+        PreloginUI preloginUI = new PreloginUI(serverFacade);
+        preloginUI.display();
     }
 }
